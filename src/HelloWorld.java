ba47836ee2d460a78f908fec973105e89e80aeeb
satish9618 <satishchandra22321@gmail.com>
2026-09-23 11:04:38 +0000

    mvn code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI Pipeline!");
+        System.out.println("Hello from Jenkins CI Pipeline SATISHHHHH!");
     }
 }
